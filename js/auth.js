7b5431b145d7b3256154e5d53a60ec8df5d183cf
Aktiar H <aktiardocuments@gmail.com>
2025-10-21 03:58:31 +0530

    dfgd

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,28 @@
-// js/auth.js
-// Simple email/password login example
+// js/auth.js - COMPLETE FIXED VERSION
 function login(email, password) {
     auth.signInWithEmailAndPassword(email, password)
-        .then(user => console.log("Logged in:", user.user.email))
-        .catch(err => console.error("Login error:", err));
+        .then((userCredential) => {
+            console.log("Logged in:", userCredential.user.email);
+            window.location.href = 'index.html';
+        })
+        .catch(err => {
+            console.error("Login error:", err);
+            alert('Login failed: ' + err.message);
+        });
 }
 
 function logout() {
     auth.signOut()
-        .then(() => console.log("Logged out"))
-        .catch(err => console.error("Logout error:", err));
+        .then(() => {
+            console.log("Logged out");
+            window.location.href = 'login.html';
+        })
+        .catch(err => {
+            console.error("Logout error:", err);
+            alert('Logout failed: ' + err.message);
+        });
 }
+
+// Make functions globally available
+window.logout = logout;
+window.login = login;
