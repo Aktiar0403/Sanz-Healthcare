475b17627248ee3f2eeed17b15ee758d0b5c2cb0
Aktiar H <aktiardocuments@gmail.com>
2025-10-22 15:21:05 +0530

    ggg

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // firebase-config.js - USING YOUR REAL FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyARxCkcyB7ts0EWoq9x9G31xhIN6fpR9kk",
+  apiKey: "AIzaSyARxCkcyB7ts0EWoq9x9G31xhIN6fpR9kk", // YOUR REAL KEY
   authDomain: "sanj-healthcare-77e02.firebaseapp.com",
-  projectId: "sanj-healthcare-77e02",
+  projectId: "sanj-healthcare-77e02", 
   storageBucket: "sanj-healthcare-77e02.firebasestorage.app",
   messagingSenderId: "710696156355",
   appId: "1:710696156355:web:13bcee177f46e3aebc7676",
   measurementId: "G-HDKXN3LZ7P"
-};
 
 // Initialize Firebase
 try {
